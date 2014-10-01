b20202c7b4d4d18b26296c58087b6e13d879a277
Beast <Beast@Beast-PC>
2014-09-30 20:29:10 -0600

    Dave UI Gen2 open source

---
+++ lakiosktest/src/com/example/lakiosktest/BitcoinDAVE.java
@@ -0,0 +1,9 @@
+package com.example.lakiosktest;
+
+import android.app.Application;
+
+public class BitcoinDAVE extends Application {
+	private static String TAG = BitcoinDAVE.class.getSimpleName();
+
+	
+}
--- lakiosktest/src/com/example/lakiosktest/MainActivity.java
+++ lakiosktest/src/com/example/lakiosktest/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.lakiosktest;
 //***************
 //Resources
 //***************
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -35,6 +38,44 @@ public static final int REDEEM_ACTIVITY = Menu.FIRST + 3;
 //***************
 //  Tools
 //***************
+
+//Dialogs
+private void openAlert() {
+	 AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+     
+	 alertDialogBuilder.setTitle(this.getTitle()+ " Hardware Failed To Start");
+	 alertDialogBuilder.setMessage("Connected Hardware failed to innitialize, would you like to try again? Or you may exit and restart the application. If this fails please power cycle the bill accetpor");
+	 // set positive button: Yes message
+	 alertDialogBuilder.setPositiveButton("Yes",new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog,int id) {
+				// go to a new activity of the app
+//				Intent positveActivity = new Intent(getApplicationContext(), .class);
+//	            startActivity(positveActivity);	
+			}
+		  });
+	 // set negative button: No message
+	 alertDialogBuilder.setNegativeButton("Return to app",new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog,int id) {
+				// cancel the alert box and put a Toast to the user
+				dialog.cancel();
+				Toast.makeText(getApplicationContext(), "You chose a negative answer", 
+						Toast.LENGTH_LONG).show();
+			}
+		});
+	 // set neutral button: Exit the app message
+	 alertDialogBuilder.setNeutralButton("Exit the app",new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog,int id) {
+				// exit the app and go to the HOME
+				MainActivity.this.finish();
+			}
+		});
+	 
+	 AlertDialog alertDialog = alertDialogBuilder.create();
+	 // show alert
+	 alertDialog.show();
+}
+
+//Activitys
 private void startLoginActivity() {
 	Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
 	startActivity(intent);
@@ -81,7 +122,6 @@ private void startSessionsActivity() {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        super.onResume();
         View mDecorView = getWindow().getDecorView();
         mDecorView.setSystemUiVisibility(
       	        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
@@ -105,7 +145,8 @@ private void startSessionsActivity() {
 				@Override
 				public void onClick(View v) {
 					//Start Scanning Page
-					startBuyerInfoActivity();
+					Toast.makeText(getApplicationContext(), "Button Pressed", Toast.LENGTH_LONG).show();
+					startBuyerSelectionActivity();
 				}
 		});
     }
